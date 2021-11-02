9fce0f9f0c08976231005c75c6ef371700bf3415
Ubuntu <ubuntu@ip-172-31-11-35.eu-west-1.compute.internal>
2021-11-02 19:00:29 +0000

    dia1

--- junit/ejemplo1/src/main/java/es/curso/App.java
+++ junit/ejemplo1/src/main/java/es/curso/App.java
@@ -7,13 +7,16 @@ public class App {
         //System.out.println( suma(-1,5) );
     }
 
-    public static int suma (int numero1, int numero2){
+    public static double suma (double numero1, double numero2){
         return numero1 + numero2;
     }
     
-    public static int funcionRara(numero1){
+    public static int funcionRara(int numero){
+        if(numero==0) return 0;
+        if(numero%2==0) return numero/2;
         // Si recibe un numero par, debe devolver la mitad
         // Si recibe un numero impar, debe devolver el doble
         // Si recibe un cero, devuelve 0
+        return numero*2;
     }
 }
--- junit/ejemplo1/src/test/java/es/curso/AppTest.java
+++ junit/ejemplo1/src/test/java/es/curso/AppTest.java
@@ -9,55 +9,104 @@ import org.junit.Test;
 public class AppTest {
     
     @Test
+    public void pruebaSuma_Decimales() {
+        double numero1=5.1;
+        double numero2=6.1;
+        double resultado_obtenido=App.suma(numero1, numero2);
+        double resultado_esperado=11.2;
+        Assert.assertEquals(resultado_esperado, resultado_obtenido, 0.000001);
+    }    
+    
+    @Test
     public void pruebaSuma_Positivos() {
         int numero1=5;
         int numero2=6;
-        int resultado_obtenido=App.suma(numero1, numero2);
+        double resultado_obtenido=App.suma(numero1, numero2);
         int resultado_esperado=11;
-        Assert.assertEquals(resultado_esperado, resultado_obtenido);
+        Assert.assertEquals(resultado_esperado, resultado_obtenido, 0.000001);
     }    
     
     @Test
     public void pruebaSuma_Negativos() {
         int numero1=-5;
         int numero2=-6;
-        int resultado_obtenido=App.suma(numero1, numero2);
+        double resultado_obtenido=App.suma(numero1, numero2);
         int resultado_esperado=-11;
-        Assert.assertEquals(resultado_esperado, resultado_obtenido);
+        Assert.assertEquals(resultado_esperado, resultado_obtenido, 0.000001);
     }    
     
     @Test
     public void pruebaSuma_PositivoYNegativo() {
         int numero1=5;
         int numero2=-6;
-        int resultado_obtenido=App.suma(numero1, numero2);
+        double resultado_obtenido=App.suma(numero1, numero2);
         int resultado_esperado=-1;
-        Assert.assertEquals(resultado_esperado, resultado_obtenido);
+        Assert.assertEquals(resultado_esperado, resultado_obtenido, 0.000001);
     }    
     
     @Test
     public void pruebaSuma_NegativoYPositivo() {
         int numero1=-5;
         int numero2=6;
-        int resultado_obtenido=App.suma(numero1, numero2);
+        double resultado_obtenido=App.suma(numero1, numero2);
         int resultado_esperado=1;
-        Assert.assertEquals(resultado_esperado, resultado_obtenido);
+        Assert.assertEquals(resultado_esperado, resultado_obtenido, 0.000001);
     }    
     
     @Test
     public void pruebaSuma_CeroPositivo() {
         int numero1=5;
         int numero2=0;
-        int resultado_obtenido=App.suma(numero1, numero2);
+        double resultado_obtenido=App.suma(numero1, numero2);
         int resultado_esperado=5;
-        Assert.assertEquals(resultado_esperado, resultado_obtenido);
+        Assert.assertEquals(resultado_esperado, resultado_obtenido, 0.000001);
     }    
     
     @Test
     public void pruebaSuma_CeroNegativo() {
         int numero1=0;
         int numero2=-6;
-        int resultado_obtenido=App.suma(numero1, numero2);
+        double resultado_obtenido=App.suma(numero1, numero2);
+        int resultado_esperado=-6;
+        Assert.assertEquals(resultado_esperado, resultado_obtenido, 0.000001);
+    }
+    
+    @Test
+    public void pruebaRara_Cero() {
+        int numero1=0;
+        int resultado_obtenido=App.funcionRara(numero1);
+        int resultado_esperado=0;
+        Assert.assertEquals(resultado_esperado, resultado_obtenido);
+    }
+    
+    @Test
+    public void pruebaRara_Par() {
+        int numero1=2;
+        int resultado_obtenido=App.funcionRara(numero1);
+        int resultado_esperado=1;
+        Assert.assertEquals(resultado_esperado, resultado_obtenido);
+    }
+    
+    @Test
+    public void pruebaRara_Impar() {
+        int numero1=5;
+        int resultado_obtenido=App.funcionRara(numero1);
+        int resultado_esperado=10;
+        Assert.assertEquals(resultado_esperado, resultado_obtenido);
+    }
+    
+    @Test
+    public void pruebaRara_ParNegativo() {
+        int numero1=-2;
+        int resultado_obtenido=App.funcionRara(numero1);
+        int resultado_esperado=-1;
+        Assert.assertEquals(resultado_esperado, resultado_obtenido);
+    }
+    
+    @Test
+    public void pruebaRara_ImparNegativo() {
+        int numero1=-3;
+        int resultado_obtenido=App.funcionRara(numero1);
         int resultado_esperado=-6;
         Assert.assertEquals(resultado_esperado, resultado_obtenido);
     }
